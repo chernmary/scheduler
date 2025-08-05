10147294c15f3f95be1b5da76a28dc30960f8503
chernmary <chernmary@gmail.com>
2025-08-05 21:02:08 +0300

    Update database.py

--- app/database.py
+++ app/database.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from app.models import Base 
+from app.db_base import Base 
 
 SQLALCHEMY_DATABASE_URL = "sqlite:///./scheduler.db"
 
