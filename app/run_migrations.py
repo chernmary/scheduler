2af19f70fd4bd343b9a008e7871163eeac295b84
chernmary <chernmary@gmail.com>
2025-08-07 22:26:25 +0300

    Update run_migrations.py

--- app/run_migrations.py
+++ app/run_migrations.py
@@ -1,13 +1,17 @@
 import os
-from alembic import command
 from alembic.config import Config
+from alembic import command
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+def run_migrations():
+    # Указываем путь к alembic.ini (он лежит в папке migrations)
+    alembic_cfg = Config(os.path.join("migrations", "alembic.ini"))
 
-alembic_cfg = Config(os.path.join(BASE_DIR, '../migrations/alembic.ini'))
+    # Получаем абсолютный путь к базе данных scheduler.db
+    project_root = os.getcwd()  # Render всегда запускает из корня проекта
+    db_path = os.path.join(project_root, "scheduler.db")
 
+    # Вставляем в конфигурацию путь к базе
+    alembic_cfg.set_main_option("sqlalchemy.url", f"sqlite:///{db_path}")
 
-db_path = os.path.join(BASE_DIR, "scheduler.db")
-alembic_cfg.set_main_option("sqlalchemy.url", f"sqlite:///{db_path}")
-
-command.upgrade(alembic_cfg, "head")
+    # Запускаем миграции
+    command.upgrade(alembic_cfg, "head")
