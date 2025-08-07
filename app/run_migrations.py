9a38c7299762125fc791781e3e30ea0a05e41284
chernmary <chernmary@gmail.com>
2025-08-07 22:12:48 +0300

    Update run_migrations.py

--- app/run_migrations.py
+++ app/run_migrations.py
@@ -1,7 +1,13 @@
 import os
-from alembic.config import Config
 from alembic import command
+from alembic.config import Config
 
-def run_migrations():
-    alembic_cfg = Config("migrations/alembic.ini")
-    command.upgrade(alembic_cfg, "head")
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+alembic_cfg = Config(os.path.join(BASE_DIR, '../migrations/alembic.ini'))
+
+
+db_path = os.path.join(BASE_DIR, "scheduler.db")
+alembic_cfg.set_main_option("sqlalchemy.url", f"sqlite:///{db_path}")
+
+command.upgrade(alembic_cfg, "head")
