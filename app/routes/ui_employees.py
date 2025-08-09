41d75ada1eda55960c8113d9e539a1a91855516a
chernmary <chernmary@gmail.com>
2025-08-09 18:40:14 +0300

    Update ui_employees.py

--- app/routes/ui_employees.py
+++ app/routes/ui_employees.py
@@ -2,10 +2,8 @@ from fastapi import APIRouter, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 
-# Шаблоны лежат в app/templates
-templates = Jinja2Templates(directory="app/templates")
-
 router = APIRouter()
+templates = Jinja2Templates(directory="app/templates")
 
 @router.get("/ui/employees", response_class=HTMLResponse)
 def employees_page(request: Request):
