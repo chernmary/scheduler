2b03d7c68893bc2c0fa883364c8f6964eeff07a1
chernmary <chernmary@gmail.com>
2025-08-13 07:20:45 +0300

    Update seed_db.py

--- app/seed_db.py
+++ app/seed_db.py
@@ -1,17 +1,13 @@
+from app.seed_locations import seed_locations
+from app.seed_employees import seed_employees
+from app.seed_employee_settings import seed_employee_settings
 
-+from app.seed_locations import seed_locations
-+from app.seed_employees import seed_employees
-+from app.seed_employee_settings import seed_employee_settings
-+
-+
-+def seed_all():
-+    seed_locations()
-+    seed_employees()
-+    seed_employee_settings()
-+
-+
-+if __name__ == "__main__":
-+    seed_all()
- 
-EOF
-)
+
+def seed_all():
+    seed_locations()
+    seed_employees()
+    seed_employee_settings()
+
+
+if __name__ == "__main__":
+    seed_all()
