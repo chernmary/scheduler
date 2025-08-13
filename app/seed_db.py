453f6da256b77a0e446d30b7e6c55a01cb4b33a3
chernmary <chernmary@gmail.com>
2025-08-13 07:19:45 +0300

    Update seed_db.py

--- app/seed_db.py
+++ app/seed_db.py
@@ -1,9 +1,4 @@
- (cd "$(git rev-parse --show-toplevel)" && git apply --3way <<'EOF' 
-diff --git a//dev/null b/app/seed_db.py
-index 0000000000000000000000000000000000000000..c32e3c631bf2a95c8aaa5325b83a046649bd9163 100644
---- a//dev/null
-+++ b/app/seed_db.py
-@@ -0,0 +1,13 @@
+
 +from app.seed_locations import seed_locations
 +from app.seed_employees import seed_employees
 +from app.seed_employee_settings import seed_employee_settings
